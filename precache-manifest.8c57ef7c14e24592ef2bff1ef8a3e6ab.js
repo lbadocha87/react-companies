2692c74940d55a3db6a671c7b826103bb29fd208
lbadocha <lukasz.badocha@gmail.com>
2020-04-28 15:44:22 +0200

    Updates

---
+++ precache-manifest.8c57ef7c14e24592ef2bff1ef8a3e6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240f4fff0727dbf3278e222c7b1a25f2",
+    "url": "/react-companies/index.html"
+  },
+  {
+    "revision": "267d94422e96bbe11e47",
+    "url": "/react-companies/static/css/main.d2503c28.chunk.css"
+  },
+  {
+    "revision": "e64371f1a690755af863",
+    "url": "/react-companies/static/js/2.74333e2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-companies/static/js/2.74333e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267d94422e96bbe11e47",
+    "url": "/react-companies/static/js/main.ff21f404.chunk.js"
+  },
+  {
+    "revision": "cbe97b15748340ffa42f",
+    "url": "/react-companies/static/js/runtime-main.a0c02e4a.js"
+  }
+]);
